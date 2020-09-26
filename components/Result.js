d27efabb0a9ea980fe4685ff6f960d7f04f126bb
윤희상 <huisang.growth@gmail.com>
2020-09-26 18:12:35 +0900

    added subtitle, optimized, gif, corrected calculator with try-catch

--- components/Result.js
+++ components/Result.js
@@ -8,13 +8,13 @@ import { ADD_URL } from "../reducers/answer";
 import { MAKE_DIRECT_TRUE } from "../reducers/answer";
 import { CLEAR_URLS } from "../reducers/answer";
 import {
-	RowsContainer,
-	GoodBadContainer,
-	WidthImg,
-	Texty,
-	Paragraphy,
-	LineBreakText,
-	GoodBadBtn,
+  RowsContainer,
+  GoodBadContainer,
+  WidthImg,
+  Texty,
+  Paragraphy,
+  LineBreakText,
+  GoodBadBtn,
 } from "../assets/css/style_result";
 import { Container, containerCol } from "../assets/css/style_global";
 
@@ -24,183 +24,171 @@ import ShareGroup from "./ShareGroup";
 import icons from "../back/icon_result";
 
 const Result = ({ name }) => {
-	const v = result[name];
-	const dispatch = useDispatch();
-	const router = useRouter();
+  const v = result[name];
+  const dispatch = useDispatch();
+  const router = useRouter();
 
-	const { url } = useSelector((state) => state.answer);
-	const { isDirect } = useSelector((state) => state.answer);
+  const { url } = useSelector((state) => state.answer);
+  const { isDirect } = useSelector((state) => state.answer);
 
-	const [thumbUrl, setThumbUrl] = useState(null);
-	const [goodTitle, setGoodTitle] = useState("");
-	const [goodId, setGoodId] = useState("");
-	const [badTitle, setBadTitle] = useState("");
-	const [badId, setBadId] = useState("");
+  const [thumbUrl, setThumbUrl] = useState(null);
+  const [goodTitle, setGoodTitle] = useState("");
+  const [goodId, setGoodId] = useState("");
+  const [badTitle, setBadTitle] = useState("");
+  const [badId, setBadId] = useState("");
 
-	useEffect(() => {
-		setThumbUrl(url["thumbUrl"]);
+  useEffect(() => {
+    setThumbUrl(url["thumbUrl"]);
 
-		setGoodTitle(result[v["good"]]["title"]);
-		setGoodId(result[v["good"]]["id"]);
-		setBadTitle(result[v["bad"]]["title"]);
-		setBadId(result[v["bad"]]["id"]);
-	}, [url]);
+    setGoodTitle(result[v["good"]]["title"]);
+    setGoodId(result[v["good"]]["id"]);
+    setBadTitle(result[v["bad"]]["title"]);
+    setBadId(result[v["bad"]]["id"]);
+  }, [url]);
 
-	useEffect(() => {
-		dispatch({ type: CLEAR_URLS });
-		if (isDirect && !thumbUrl) {
-			console.log("[isDirect]");
+  useEffect(() => {
+    dispatch({ type: CLEAR_URLS });
+    if (isDirect && !thumbUrl) {
+      console.log("[isDirect]");
 
-			storage
-				.ref()
-				.child(v["thumbnail"])
-				.getDownloadURL()
-				.then((url) => {
-					setThumbUrl(url);
-				})
-				.catch((err) => {
-					return;
-				});
-		} else {
-			dispatch({
-				type: MAKE_DIRECT_TRUE,
-			});
-		}
-	}, [isDirect]);
+      storage
+        .ref()
+        .child(v["thumbnail"])
+        .getDownloadURL()
+        .then((url) => {
+          setThumbUrl(url);
+        })
+        .catch((err) => {
+          return;
+        });
+    } else {
+      dispatch({
+        type: MAKE_DIRECT_TRUE,
+      });
+    }
+  }, [isDirect]);
 
-	useEffect(() => {
-		if (isDirect && thumbUrl) {
-			dispatch({
-				type: ADD_URL,
-				data: {
-					thumbUrl: thumbUrl,
-				},
-			});
-		}
-	}, [thumbUrl]);
+  useEffect(() => {
+    if (isDirect && thumbUrl) {
+      dispatch({
+        type: ADD_URL,
+        data: {
+          thumbUrl: thumbUrl,
+        },
+      });
+    }
+  }, [thumbUrl]);
 
-	const toGoodBad = (id) => {
-		if (id === "") {
-			return;
-		}
-		router.push("/result/".concat(id));
-		dispatch({ type: CLEAR_URLS });
-	};
+  const toGoodBad = (id) => {
+    if (id === "") {
+      return;
+    }
+    router.push("/result/".concat(id));
+    dispatch({ type: CLEAR_URLS });
+  };
 
-	return (
-		<>
-			<Head>
-				<script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>
-			</Head>
-			<Container top={-10}>
-				<Col {...containerCol}>
-					<WidthImg src={icons["top"]} />
+  return (
+    <>
+      <Head>
+        <script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>
+      </Head>
+      <Container top={-10}>
+        <Col {...containerCol}>
+          <WidthImg src={icons["top"]} />
 
-					<Texty
-						strong
-						top={10}
-						font={27}
-						style={{ fontFamily: "Godo", fontWeight: "bold" }}
-					>
-						{v["title_1"]}
-					</Texty>
-					<Texty
-						strong
-						top={-1}
-						font={33}
-						style={{ fontFamily: "Godo", fontWeight: "bold" }}
-					>
-						{v["title_2"]}
-					</Texty>
+          <Texty
+            strong
+            top={10}
+            font={27}
+            style={{ fontFamily: "Godo", fontWeight: "bold" }}
+          >
+            {v["title_1"]}
+          </Texty>
+          <Texty
+            strong
+            top={-1}
+            font={33}
+            style={{ fontFamily: "Godo", fontWeight: "bold" }}
+          >
+            {v["title_2"]}
+          </Texty>
 
-					<WidthImg
-						src={icons[v["id"]]}
-						style={{
-							width: "90%",
-						}}
-					/>
+          <WidthImg
+            src={icons[v["id"]]}
+            style={{
+              width: "90%",
+            }}
+          />
 
-					<Texty
-						strong
-						top={10}
-						font={25}
-						style={{ fontFamily: "NanumPen" }}
-					>
-						{v["subtitle"]}
-					</Texty>
+          <Texty strong top={10} font={25} style={{ fontFamily: "NanumPen" }}>
+            "{v["subtitle"]}"
+          </Texty>
 
-					<Paragraphy top={10} font={15}>
-						{v["description"]}
-					</Paragraphy>
+          <Paragraphy top={10} font={15}>
+            {v["description"]}
+          </Paragraphy>
 
-					<WidthImg
-						src={icons["line_horizontal"]}
-						style={{ marginTop: "20px", marginBottom: "20px" }}
-					/>
+          <WidthImg
+            src={icons["line_horizontal"]}
+            style={{ marginTop: "20px", marginBottom: "20px" }}
+          />
 
-					<RowsContainer>
-						<GoodBadContainer>
-							<Texty
-								font={15}
-								strong
-								style={{
-									fontFamily: "Godo",
-									fontWeight: "bold",
-								}}
-							>
-								잘 맞는 친구
-							</Texty>
+          <RowsContainer>
+            <GoodBadContainer>
+              <Texty
+                font={15}
+                strong
+                style={{
+                  fontFamily: "Godo",
+                  fontWeight: "bold",
+                }}
+              >
+                잘 맞는 친구
+              </Texty>
 
-							<GoodBadBtn onClick={() => toGoodBad(goodId)}>
-								<LineBreakText font={20}>
-									{goodTitle}
-								</LineBreakText>
-								<WidthImg src={icons[goodId]} />
-							</GoodBadBtn>
-						</GoodBadContainer>
+              <GoodBadBtn onClick={() => toGoodBad(goodId)}>
+                <LineBreakText font={20}>{goodTitle}</LineBreakText>
+                <WidthImg src={icons[goodId]} />
+              </GoodBadBtn>
+            </GoodBadContainer>
 
-						<GoodBadContainer width={4}>
-							<img
-								src={icons["line_vertical"]}
-								style={{ height: "200px" }}
-							/>
-						</GoodBadContainer>
+            <GoodBadContainer width={4}>
+              <img src={icons["line_vertical"]} style={{ height: "200px" }} />
+            </GoodBadContainer>
 
-						<GoodBadContainer>
-							<Texty
-								font={15}
-								strong
-								style={{
-									fontFamily: "Godo",
-									fontWeight: "bold",
-								}}
-							>
-								어색한 친구
-							</Texty>
+            <GoodBadContainer>
+              <Texty
+                font={15}
+                strong
+                style={{
+                  fontFamily: "Godo",
+                  fontWeight: "bold",
+                }}
+              >
+                어색한 친구
+              </Texty>
 
-							<GoodBadBtn onClick={() => toGoodBad(badId)}>
-								<LineBreakText font={20}>
-									{badTitle}
-								</LineBreakText>
-								<WidthImg src={icons[badId]} />
-							</GoodBadBtn>
-						</GoodBadContainer>
-					</RowsContainer>
+              <GoodBadBtn onClick={() => toGoodBad(badId)}>
+                <LineBreakText font={20}>{badTitle}</LineBreakText>
+                <WidthImg src={icons[badId]} />
+              </GoodBadBtn>
+            </GoodBadContainer>
+          </RowsContainer>
 
-					<div style={{ height: "20px" }}></div>
+          <div style={{ height: "20px" }}></div>
 
-					<ShareGroup
-						title={v["title"] ? v["title"] : ""}
-						thumbUrl={thumbUrl ? thumbUrl : ""}
-					/>
+          <ShareGroup
+            title={v["title"] ? v["title"] : ""}
+            thumbUrl={thumbUrl ? thumbUrl : ""}
+          />
 
-					<div style={{ height: "50px" }}></div>
+          <div style={{ height: "50px" }}></div>
 
-					<Footer />
-				</Col>
-			</Container>
-		</>
-	);
+          <Footer />
+        </Col>
+      </Container>
+    </>
+  );
 };
 
 export default Result;
--- pages/test.js
+++ pages/test.js
@@ -13,58 +13,58 @@ import { Container, containerCol } from "../assets/css/style_global";
 import { Col } from "antd";
 
 const Test = () => {
-	const router = useRouter();
-	const dispatch = useDispatch();
+  const router = useRouter();
+  const dispatch = useDispatch();
 
-	const maxCount = list.arr.length;
-	const [answerArr, setAnswerArr] = useState([]);
-	const [index, setIndex] = useState(1);
+  const maxCount = list.arr.length;
+  const [answerArr, setAnswerArr] = useState([]);
+  const [index, setIndex] = useState(1);
 
-	const handler = (ans) => {
-		setAnswerArr((prevArr) => [...prevArr, ans]);
+  const handler = (ans) => {
+    setAnswerArr((prevArr) => [...prevArr, ans]);
 
-		if (index === 10) {
-			ans === 1
-				? setIndex(11)
-				: (setAnswerArr((prevArr) => [...prevArr, 9]), setIndex(12));
-		} else if (index === 11) {
-			setAnswerArr((prevArr) => [...prevArr, 9]);
-			setIndex(13);
-		} else {
-			if (index === maxCount) {
-				router.push("/loading");
-				return;
-			}
-			setIndex(index + 1);
-		}
-	};
+    if (index === 10) {
+      ans === 0
+        ? setIndex(11)
+        : (setAnswerArr((prevArr) => [...prevArr, 9]), setIndex(12));
+    } else if (index === 11) {
+      setAnswerArr((prevArr) => [...prevArr, 9]);
+      setIndex(13);
+    } else {
+      if (index === maxCount) {
+        router.push("/loading");
+        return;
+      }
+      setIndex(index + 1);
+    }
+  };
 
-	useEffect(() => {
-		if (answerArr.length === maxCount) {
-			console.log("AnswerArr is: " + answerArr);
-			dispatch({ type: CLEAR_URLS });
-			dispatch({
-				type: ADD_ANSWER,
-				data: answerArr,
-			});
-		}
-	});
+  useEffect(() => {
+    if (answerArr.length === maxCount) {
+      console.log("AnswerArr is: " + answerArr);
+      dispatch({ type: CLEAR_URLS });
+      dispatch({
+        type: ADD_ANSWER,
+        data: answerArr,
+      });
+    }
+  });
 
-	return (
-		<>
-			<ContainerExtended>
-				<Col {...containerCol}>
-					<Question index={index} handler={handler} />
-					<ProgressBar i={list.arr[index - 1].progress} length={12} />
-				</Col>
-			</ContainerExtended>
-		</>
-	);
+  return (
+    <>
+      <ContainerExtended>
+        <Col {...containerCol}>
+          <Question index={index} handler={handler} />
+          <ProgressBar i={list.arr[index - 1].progress} length={12} />
+        </Col>
+      </ContainerExtended>
+    </>
+  );
 };
 
 const ContainerExtended = styled(Container)`
-	width: 80%;
-	transform: translateX(10%);
+  width: 80%;
+  transform: translateX(10%);
 `;
 
 export default Test;
